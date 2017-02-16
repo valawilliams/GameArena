406e4293828b2bf162edd4309eb3d3371f7e7e5f
Lewis <lewisd4@lancs.local>
2017-02-16 02:41:53 +0000

    - Ammended South Boarder to thinner Red line, Added missedLine Variable, Fixed PlayArea Bugs, Tested with Ball size 12 diameter which equates to Rectangle dimensions (24,24) , Updated Index.

--- Gameplay.java
+++ Gameplay.java
@@ -13,12 +13,12 @@ public class Gameplay {
 	 * @param GAHMid Calculates the Central Coordinate of the GameArena on the Y-Axis
 	 * @param GAWMin Calculates the Minimum Coordinate of the GameArena on the X-Axis
 	 * @param GAHMin Calculates the Minimum Coordinate of the GameArena on the Y-Axis
-	 * @param EdgesX Calculates the space in the GameArena which is used for the boarders at the GAWMin and GAWMax and to Calculate the Play Area (PWMin & PWMax)
-	 * @param EdgesY Calculates the space in the GameArena which is used for the boarders at the GAHMin and GAHMax and to Calculate the Play Area (PHMin & PHMax)
-	 * @param PWMax Calculates the Playable Width Max Coordinate on the X-Axis Minus the Boarder (GAWMax-EdgesX)
-	 * @param PHMAX Calculates the Playable Width Max Coordinate on the Y-Axis Minus the Boarder (GAHMax-EdgesY)
-	 * @param PWMid Calculates the Central Coordinate of the Playable Area on the X-Axis (PWMax / 2)
-	 * @param PHMid Calculates the Central Coordinate of the Playable Area on the Y-Axis (PHMax /2)
+	 * @param EdgesX Calculates the space in the GameArena which is used for the boarders at the GAWMin and GAWMax and to Calculate the Play Area
+	 * @param EdgesY Calculates the space in the GameArena which is used for the boarders at the GAHMin and GAHMax and to Calculate the Play Area
+	 * @param PWMax Calculates the Playable Width Max Coordinate on the X-Axis Minus the Boarder
+	 * @param PHMAX Calculates the Playable Width Max Coordinate on the Y-Axis Minus the Boarder
+	 * @param PWMid Calculates the Central Coordinate of the Playable Area on the X-Axis
+	 * @param PHMid Calculates the Central Coordinate of the Playable Area on the Y-Axis
 	 * @param PWMin Calculates the Minimum Coordinate of the Playable Area on the X-Axis
 	 * @param PHMin Calculates the Minimum Coordinate of the Playable Area on the Y-Axis
 	 * @param Nboard Creates a Rectangle to form a Border at the TOP of the GameArena
@@ -32,34 +32,35 @@ public class Gameplay {
 		GameArena arena = new GameArena (1000,1000);
 		double GAWMax=arena.getArenaWidth (); // GameArena Max Width
 		double GAHMax=arena.getArenaHeight (); // GameArena Max Height
-		double GAWMid=arena.getArenaWidth() / 2; //GameArena Midpoint on X axis
-		double GAHMid=arena.getArenaHeight() / 2; //Game Arena Midpoint on Y Axis
+		double GAWMid=arena.getArenaWidth()/2; //GameArena Midpoint on X axis
+		double GAHMid=arena.getArenaHeight()/2; //Game Arena Midpoint on Y Axis
 		double GAWMin=GAWMax-GAWMax; // GameArena Min Co-ordinate on X Axis
-		double GAHMin=GAHMax-GAWMax; // GameArena Min Co-ordinate on Y Axis
-		double EdgesX=(GAWMax*0.03); //X edges are 3% of total Gamearea
-		double EdgesY=(GAHMax *0.08); // Y edges are 8% of total Gamearea
+		double GAHMin=GAHMax-GAHMax; // GameArena Min Co-ordinate on Y Axis
+		double EdgesX=GAWMax*0.03; //X edges are 3% of total Gamearea
+		double EdgesY=GAHMax*0.08; // Y edges are 8% of total Gamearea
+		double missedLine = GAHMax -25; //Red line at Bottom of GameArena
 	
 		// Calculate size of PlayArena
 		double PWMax=GAWMax-EdgesX; // Max Play Arena Width
-		double PHMax=GAHMax-EdgesY; // Max Play Height
-		double PWMid=PWMax/2; // Middle Of Play Width
-		double PHMid=PHMax/2; // Middle Of Play Height
+		double PHMax=GAHMax-20; // Max Play Height
 		double PWMin=GAWMin+EdgesX; // Min play Area
-		double PWHMin=GAHMin+EdgesY; // Max Play Area
-		
+		double PHMin=(GAHMin+(EdgesY-25)); // Max Play Area
+		double PWMid=PWMax*0.5; // Middle Of Play Width
+		double PHMid=PHMax*0.5; // Middle Of Play Height
 		
 		
 		// Draw Boarder	
-		Rectangle Sboard= new Rectangle(GAWMid,GAHMax,GAWMax,EdgesY,"#999999",0,0); //Bottom Border
 		Rectangle Wboard= new Rectangle(GAWMin,GAHMid,EdgesX,GAHMax,"#999999",0,0); // Right Boarder
 		Rectangle Nboard = new Rectangle(GAWMid,GAHMin,GAWMax,EdgesY,"#999999",0,0); // Top Boarder
 		Rectangle Eboard = new Rectangle(GAWMax,GAHMid,EdgesX,GAHMax,"#999999",0,0); // Left Boarder
+		Rectangle Sboard = new Rectangle(GAWMid,GAHMax,PWMax,10,"#CC1100",0,0); // Bottom Boarder
 		arena.addRectangle(Nboard);
 		arena.addRectangle(Eboard);
 		arena.addRectangle(Sboard);
 		arena.addRectangle(Wboard);
-		
-	}
+
+		//Dropped Items Test Size = Ball Diameter 12 which equates to (24,24) Rectangle 
+		}
 
 }
 
